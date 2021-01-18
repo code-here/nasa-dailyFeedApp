5dd4383ff008e8a82c52396ddb3f0578c2ae87e5
Abhinav <abhinavy14@gmail.com>
2021-01-18 21:09:15 +0530

    another commit

--- getDaily.js
+++ getDaily.js
@@ -1,6 +1,8 @@
 const axios = require('axios').default;
 
-const baseurl = "https://api.nasa.gov/planetary/apod?api_key=VpLegQawyYVKrij4PAm84X7EgNvn7DDiaeo6UF3g";
+const api_key = process.env.APOD_API_KEY;
+
+const baseurl = `https://api.nasa.gov/planetary/apod?api_key=${api_key}`;
 
 
 const dailyData = (callback,date = false, start_date = false, end_date = false) => {
